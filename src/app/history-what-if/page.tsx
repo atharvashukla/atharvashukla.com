0fd7429b01f57e9afb3cb5fd4f93968a01133913
Atharva <atharvashukla282@gmail.com>
2024-03-05 00:40:26 +0530

    history

---
+++ src/app/history-what-if/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>History - on one hand - doesn't allow for what-ifs. On the other hand, History is full of what-ifs.</div>
+  )
+}
+
+export default page
