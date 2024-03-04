d6f54ede269cc97634a7c10357515e72881caebc
Atharva <atharvashukla282@gmail.com>
2024-03-05 00:43:05 +0530

    update

---
+++ src/app/history-and-what-ifs/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+let str = "History - on one hand - doesn't allow for what-ifs. On the other hand, History is full of what-ifs."
+
+function page() {
+  return (
+    <article className='prose-'>{str}</article>
+  )
+}
+
+export default page
--- src/app/history-what-if/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function page() {
-  return (
-    <div>History - on one hand - doesn't allow for what-ifs. On the other hand, History is full of what-ifs.</div>
-  )
-}
-
-export default page
